7ae5ce5b73ac3e522a93f75517e5f143dee85b29
sungmin <sungmin5565waxfds@naver.com>
2024-03-25 14:16:05 +0900

    abcd

---
+++ src/main/java/Code4_13.java
@@ -0,0 +1,18 @@
+public class Code4_13 {
+    public static void main(String[] args) {
+        String str= "    한글 ABCD efgh";
+        String upper = "";
+        String lower = "";
+        String cutStr = "";
+
+        upper = str.toUpperCase();
+        lower = str.toLowerCase();
+        cutStr = str.trim();
+
+        System.out.println("기존 문자열 ==> [" + str + "]");
+        System.out.println("대문자 ==> ["+upper+"]");
+        System.out.println("공백 제거 ==> [" + cutStr + "]");
+        System.out.println("소문자 ==> ["+lower+"]");
+
+    }
+}
