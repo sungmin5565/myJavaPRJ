d3278c1e00c6c9702f0c5aa32967bb093a959f12
sungmin <sungmin5565waxfds@naver.com>
2024-04-01 08:55:55 +0900

    블랙

---
+++ src/main/java/Lab04_01.java
@@ -0,0 +1,12 @@
+public class Lab04_01 {
+    public static void main(String[] args) {
+        String ss = "블랙핑크";
+        System.out.println("원본 문자열 ==> " +ss);
+
+        System.out.println("반대 문자열 ==> ");
+        System.out.println(ss.charAt(3));
+        System.out.println(ss.charAt(2));
+        System.out.println(ss.charAt(1));
+        System.out.println(ss.charAt(0));
+    }
+}
