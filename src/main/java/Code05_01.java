3d4f24b2273ac34864f6eeefdbd2a74d04da3324
sungmin <sungmin5565waxfds@naver.com>
2024-04-01 13:40:17 +0900

    응깃

---
+++ src/main/java/Code05_01.java
@@ -0,0 +1,14 @@
+import java.util.concurrent.SynchronousQueue;
+
+public class Code05_01 {
+    public static void main(String[] args) {
+        int num = 99;
+
+        if (num < 100) {
+            System.out.println("100보다");
+            System.out.println("작습니다");
+        }
+
+
+    }
+}
