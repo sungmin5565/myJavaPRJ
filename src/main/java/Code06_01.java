3ccafcb0845faae3f4defd24c4b48830979456b2
sungmin <sungmin5565waxfds@naver.com>
2024-04-08 08:34:06 +0900

    으샤으샤

---
+++ src/main/java/Code06_01.java
@@ -0,0 +1,7 @@
+public class Code06_01 {
+    public static void main(String[] args) {
+        System.out.println("으샤으샤");
+        System.out.println("으샤으샤");
+        System.out.println("으샤으샤");
+    }
+}
