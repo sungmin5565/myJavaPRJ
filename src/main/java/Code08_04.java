d4902dbfc8a45e0ed6af15c26bbf767f4faae138
sungmin <sungmin5565waxfds@naver.com>
2024-05-13 15:21:09 +0900

    rabbit4

---
+++ src/main/java/Code08_04.java
@@ -0,0 +1,16 @@
+class Rabbit3 {
+    Rabbit3 () {
+        System.out.println("1. 슈퍼 클래스(토끼)의 생성자가 호출됩니다.");
+    }
+}
+class HouseRabbit3 extends Rabbit3 {
+    HouseRabbit3 () {
+        System.out.println("2. 서브 클래스(집토끼)의 생성자가 호출됩니다.");
+    }
+}
+
+public class Code08_04 {
+    public static void main(String[] args) {
+        HouseRabbit3 hRabbit3 = new HouseRabbit3();
+    }
+}
