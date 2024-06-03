cb01bc242884fa830772edab41366f7c50142ba6
sungmin <sungmin5565waxfds@naver.com>
2024-06-03 12:15:59 +0900

    0102123123123454

---
+++ src/main/java/Code09_07.java
@@ -0,0 +1,8 @@
+public class Code09_07 {
+    public static void main(String[] args) {
+        int[] ary = {10, 20, 30};
+        for (int data : ary) {
+            System.out.println(data);
+        }
+    }
+}
